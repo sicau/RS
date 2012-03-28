95d675267ac945c58d96147f6eca88dc0d32ad26
liu <th.swby@gmail.com>
2012-03-28 18:17:30 +0800

    work by liu

--- src/cn/edu/sicau/rs/function/Picture.java
+++ src/cn/edu/sicau/rs/function/Picture.java
@@ -1,6 +1,7 @@
 package cn.edu.sicau.rs.function;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.util.Iterator;
 import java.util.List;
 
@@ -15,13 +16,13 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import cn.edu.sicau.rs.exception.PictureErrorException;
 
 public class Picture {
-	public boolean uploadImg(HttpServletRequest request, String id){
+	public boolean uploadImg(HttpServletRequest request, String id) throws UnsupportedEncodingException{
 		
 		final long MAX_SIZE = 3*1024*1024;
 		
 		DiskFileItemFactory dfif = new DiskFileItemFactory();   
 	    dfif.setSizeThreshold(4096);  
-	    dfif.setRepository(new File("E://Picture"));
+	    dfif.setRepository(new File("D://upload"));
 	    
 	    ServletFileUpload sfu = new ServletFileUpload(dfif);
 	    sfu.setSizeMax(MAX_SIZE);
@@ -38,39 +39,44 @@ public class Picture {
 	    if(fileList == null || fileList.size() == 0) {
     		throw new PictureErrorException("上传图片错误，没有选择图片！");
 	    }
-	    
 	    Iterator fileItr = fileList.iterator();
 	    while (fileItr.hasNext()) {
 	    	FileItem fileItem = null;
-	   
 	    	String path = null;
 	    	long size = 0;
 	    	fileItem = (FileItem) fileItr.next();
 	    	if(fileItem == null || fileItem.isFormField()) {
-	    		continue;    //忽略非type=“file”字段
+	    		String formName = fileItem.getString();
+	    		String value = fileItem.getFieldName();
+	    		value = new String(value.getBytes(), "gb2312");
+	    		formName = new String(formName.getBytes(), "gb2312"); 
+	    		System.out.println(formName);
+	    		//continue;    //忽略非type=“file”字段
+	    	}else {
+	    		path = fileItem.getName();     //得到文件的完整路径
+		    	size = fileItem.getSize();     //get file size
+		    	if("".equals(path)||size == 0) {
+		    		throw new PictureErrorException("上传有误！");
+		    	}
+		    	String t_name = path.substring(path.lastIndexOf("\\") + 1);
+		    	String t_ext = t_name.substring(t_name.lastIndexOf(".") + 1);
+		    	
+		    	long now = System.currentTimeMillis();
+		    	String prefix = String.valueOf(now);
+		    	String paths = "D:/upload/";  // 指定图片存放目录
+		    	File file = new File(paths);
+		    	file.mkdir();
+		    	String u_name = now+"."+t_ext;    //重命名
+		    	try {
+		    		fileItem.write(new File(paths, u_name));
+		    		System.out.println(u_name + size );
+		    		return true;
+		    	} catch(Exception e) {
+		    		e.printStackTrace();
+		    	}
 	    	}
 	    	  
-	    	path = fileItem.getName();     //得到文件的完整路径
-	    	size = fileItem.getSize();     //get file size
-	    	if("".equals(path)||size == 0) {
-	    		throw new PictureErrorException("上传有误！");
-	    	}
-	    	String t_name = path.substring(path.lastIndexOf("\\") + 1);
-	    	String t_ext = t_name.substring(t_name.lastIndexOf(".") + 1);
-	    	
-	    	long now = System.currentTimeMillis();
-	    	String prefix = String.valueOf(now);
-	    	String paths = "E:/Photos/";  // 指定图片存放目录
-	    	File file = new File(paths);
-	    	file.mkdir();
-	    	String u_name = now+"."+t_ext;    //重命名
-	    	try {
-	    		fileItem.write(new File(paths, u_name));
-	    		System.out.println(u_name + size );
-	    		return true;
-	    	} catch(Exception e) {
-	    		e.printStackTrace();
-	    	}
+		    	
 	    }
 	    return false;
 	}
